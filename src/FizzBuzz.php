e6245b60fb5cc98e6e8b4ed1531a9e1d94b093c1
Takashi Kanemoto <kanemoto.takashi@gmail.com>
2015-03-15 02:18:50 +0900

    impl FizzBuzz class

--- src/FizzBuzz.php
+++ src/FizzBuzz.php
@@ -1,11 +1,26 @@
 <?php
-/**
- * This file is part of the Qck.FizzBuzz
- *
- * @license http://opensource.org/licenses/bsd-license.php BSD
- */
+
 namespace Qck\FizzBuzz;
+
+use Qck\FizzBuzz\Iterator\NumberIterator;
+use Qck\FizzBuzz\Resolver\ResolverInterface;
 
 class FizzBuzz
 {
+    private $resolver;
+
+    public function __construct(ResolverInterface $resolver)
+    {
+        $this->resolver = $resolver;
+    }
+
+    public function run(NumberIterator $numbers)
+    {
+        $results = [];
+        foreach ($numbers as $number) {
+            $results[] = $this->resolver->resolve($number);
+        }
+
+        return $results;
+    }
 }
